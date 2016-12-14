5a9a841c78724940ac0ed60d41f303dd500e2069
Guillaume Hillion <Guillaume Hillion>
2016-12-14 17:33:05 +0100

    Mise à jour du SQL, premiers tests d'implémentation

--- ImplémentationSQL.sql
+++ ImplémentationSQL.sql
@@ -1,11 +1,31 @@
 
+DROP TABLE TypePokemon CASCADE;
+DROP TABLE EspecePokemon CASCADE;
+DROP TABLE IndividuPokemon CASCADE;
+DROP TABLE PokemonSauvage CASCADE;
+DROP TABLE PokemonCapture CASCADE;
+DROP TABLE Joueur CASCADE;
+DROP TABLE Arene CASCADE;
+DROP TABLE Pokestop CASCADE;
+DROP TABLE Shop CASCADE;
+DROP TABLE Objet CASCADE;
+DROP TABLE Potion CASCADE;
+DROP TABLE Pokemon CASCADE;
+DROP TABLE ParametresAdmin CASCADE;
+DROP TABLE CombattreDans CASCADE;
+DROP TABLE Connaitre CASCADE;
+DROP TABLE Visiter CASCADE;
+DROP TABLE Vendre CASCADE;
+DROP TABLE Posseder CASCADE;
+DROP TABLE Proposer CASCADE;
+DROP TABLE EffectuerTransactionAvec CASCADE;
 
+BEGIN;
 
-
-create table TypePokemon(
+CREATE TABLE TypePokemon(
 	nom varchar primary key);
 	
-create table EspecePokemon(
+CREATE TABLE EspecePokemon(
 	nom varchar primary key,
 	numFamille integer,
 	probaApparition float,
@@ -20,7 +40,7 @@ create table EspecePokemon(
 	CHECK(probaApparition>=0 AND probaApparition<=1 AND probaCapture>=0 AND probaCapture<=1)
 	);
 
-create table IndividuPokemon(
+CREATE TABLE IndividuPokemon(
 	nom varchar references EspecePokemon(nom),
 	num integer,
 	attaqueIV float,
@@ -28,30 +48,30 @@ create table IndividuPokemon(
 	santeIV float,
 	capaciteCombat float,
 	objetPorte varchar references Objet(nom),
-	PRIMARY KEY(nom,num)
+	PRIMARY KEY(nom,num),
 	CHECK(attaqueIV>=0 AND attaqueIV<=15 AND defenseIV>=0 AND defenseIV<=15 AND santeIV>=0 AND santeIV<=15)
 	);
 
-####19####
-create table pokemonSauvage(
+
+CREATE TABLE PokemonSauvage(
 	nom varchar references IndividuPokemon(nom),
 	num integer references IndividuPokemon(num),
 	coord_lattitude float,
 	coord_longitude float,
-	unique(coord_lattitude, coord_longitude)
-	primary key(nom,num),
+	unique(coord_lattitude, coord_longitude),
+	primary key(nom,num)
 );
-####19####
-create table pokemonCapture(
+
+CREATE TABLE PokemonCapture(
 	nom varchar references IndividuPokemon(nom),
 	num integer references IndividuPokemon(num),
 	dresseur varchar not null references joueur(nom),
 	pokeball varchar not null references Pokeball(nom),
-	primary key(nom,num),
+	primary key(nom,num)
 );
 
-####3####
-create table joueur(
+
+CREATE TABLE Joueur(
 	nom varchar primary key,
 	email varchar unique,
 	dateNaissance date not null,
@@ -63,35 +83,35 @@ create table joueur(
 	nbPokestopsVisitesAjd integer,
 	nbPokemonsCapturesAjd integer,
 	derniereConnexion date,
-	pokeCoins integer, #Oubli dans le MLD
-	argent float, #Oubli dans le MLD
+	pokeCoins integer,
+	argent float,
 	unique(coord_lattitude, coord_longitude),
 	check(genre = "masculin" or genre = "feminin")
 	
 );
 
-####4#####
-create table Arene(
+
+CREATE TABLE Arene(
 	nom varchar primary key,
 	photo varchar unique,
 	coord_lattitude float,
 	coord_longitude float,
 	unique(coord_lattitude, coord_longitude)
 );
-####5######
-create table Pokestop(
+
+CREATE TABLE Pokestop(
 	nom varchar primary key,
 	photo varchar unique,
 	coord_lattitude float,
 	coord_longitude float,
 	unique(coord_lattitude, coord_longitude)
 );
-####6######
-create table Shop(
+
+CREATE TABLE Shop(
 	pays varchar primary key
 );
 
-create table Objet(
+CREATE TABLE Objet(
 	nom varchar primary key,
 	type varchar not null,
 	description varchar,
@@ -99,23 +119,23 @@ create table Objet(
 	check(type = "achetable" or type="trouvable" or type="donne")
 );
 
-create table Potion(
+CREATE TABLE Potion(
 	nom varchar primary key references Objet(nom),
 	santeRestauree integer not null
 );
 
-create table Pokeball(
+CREATE TABLE Pokeball(
 	nom varchar primary key references Objet(nom),
 	type varchar,
-	check(type = "classique" or type="artisanale"
+	check(type = "classique" or type="artisanale")
 );
 
 CREATE TABLE ParametresAdmin(
 	distanceMaxPokestop integer,
 	distanceMaxPokemon integer,
 	maxCapture integer,
-	maxPokestopsVisitables integer,
-	PRIMARY KEY(distanceMaxPokestop,distanceMaxPokemon,maxCapture,maxPokestopsVisitables)
+	maxPokestopsVisiTABLEs integer,
+	PRIMARY KEY(distanceMaxPokestop,distanceMaxPokemon,maxCapture,maxPokestopsVisiTABLEs)
 );
 
 CREATE TABLE CombattreDans(
@@ -138,29 +158,31 @@ CREATE TABLE  Visiter(
 );
 
 CREATE TABLE Vendre(
-	shop REFERENCES Shop(pays),
-	objet REFERENCES Objet(nom),
+	shop varchar references Shop(pays),
+	objet varchar references Objet(nom),
 	PRIMARY KEY(shop,objet)
 );
 
 CREATE TABLE Posseder(
-	joueur REFERENCES Joueur(nom),
-	objet REFERENCES Objet(nom),
+	joueur varchar references Joueur(nom),
+	objet varchar references Objet(nom),
 	quantite integer NOT NULL,
 	PRIMARY KEY(joueur,objet)
 );
 
 CREATE TABLE Proposer(
-	pokestop REFERENCES Pokestop(nom),
-	objet REFERENCES Objet(nom),
+	pokestop varchar references Pokestop(nom),
+	objet varchar references Objet(nom),
 	quantite integer NOT NULL,
 	PRIMARY KEY(joueur,objet)
 );
 
 CREATE TABLE EffectuerTransactionAvec(
-	shop REFERENCES Shop(pays),
-	joueur REFERENCES Joueur(nom),
+	shop varchar references Shop(pays),
+	joueur varchar references Joueur(nom),
 	date date,
 	argentDepense float,
 	PRIMARY KEY(shop,joueur)
 );
+
+COMMIT;
