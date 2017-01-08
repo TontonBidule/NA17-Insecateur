a2b9fb3a1a738ef3230452abd0522e2142e38dac
bwjtkw <bwojtkow@etu.utc.fr>
2017-01-08 10:37:19 +0100

    shop definitif (theoriquement)

--- public_html/achatShop.php
+++ public_html/achatShop.php
@@ -1,36 +1,24 @@
 
 
-<?php
 
-include('connexionBDD.php');
+<form action = "achatShopVerif1.php" method = "POST">
+	Choisissez un objet à acheter : <SELECT name="nomObjet" size="1">
+		<?php
+			include('connexionBDD.php');
 			$pseudo = "Arobaz";
 			$vSql = "SELECT o.nom, prixargentreel(o.nom)  
 					FROM Objet o, Vendre v, joueur j, shop s 
 					WHERE o.type='achetable' AND v.objet = o.nom AND v.shop = s.pays AND j.pays = s.pays;";
 			$vQuery = pg_query($vConn, $vSql);
-			$vResult = pg_fetch_array($vQuery);
-
-
-	
-			
-			if (empty($vResult))
-			{
-				echo "Pas d'objets a vendre dans ce shop";
-				echo "<br>";
-			}
-			else
-			{
-			echo '<form action = "achatShopVerif1.php" method = "POST">';
-			echo 'Choisissez un objet a acheter : <SELECT name="nomObjet" size="1">';
-			do{ 
-		
-			echo '<OPTION value = " \"$vResult[nom]\" > $vResult[nom] : $vResult[prixargentreel] pc"';}
+			while($vResult = pg_fetch_array($vQuery)){ 
+		?>	
+				<OPTION value =<?php echo "\"$vResult[nom]\" > $vResult[nom] : $vResult[prixargentreel] pc"?>
 				
-			while($vResult = pg_fetch_array($vQuery));
-			
-	echo "</SELECT>";
+		<?php
+		};
+		?>
+	</SELECT>
 
-	echo 'entrez la quantite : <input type ="number", name = "nombre, min=0" />';
-	echo '<input type ="submit" value="valider" />';
-			echo "</form>";}
-?>
+	entrez la quantité : <input type ="number", name = "nombre" />
+	<input type ="submit" value="valider" />
+</form>
